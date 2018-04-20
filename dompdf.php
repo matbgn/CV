8362a8ebfa3a457dd6de9950796e3b17cef52c68
Matthieu Borgognon <matrash@bqn.ch>
2018-04-20 22:49:46 +0200

    Ratio 197

--- dompdf.php
+++ dompdf.php
@@ -13,7 +13,7 @@ $dompdf->loadHtml(file_get_contents('index.html'));
 $dompdf->setPaper('A4', 'portrait');
 
 // Other options
-$dompdf->set_option('dpi', '196', 'pdfBackend', 'GD');
+$dompdf->set_option('dpi', '197');
 
 // Render the HTML as PDF
 $dompdf->render();
