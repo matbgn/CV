008344250192aca6315f1e88617a547e5350f69b
Matthieu Borgognon <matrash@bqn.ch>
2018-04-20 19:29:26 +0200

    Added index.html as input for dompdf

--- dompdf.php
+++ dompdf.php
@@ -7,10 +7,10 @@ use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+$dompdf->loadHtml(file_get_contents('index.html'));
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'portrait');
+$dompdf->setPaper(array(0,0,850,1600), 'portrait');
 
 // Render the HTML as PDF
 $dompdf->render();
