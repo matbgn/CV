099e5cce5e9cebee79b33024e5789ffc7b313560
Matthieu Borgognon <matrash@bqn.ch>
2018-04-20 21:47:16 +0200

    Scale document dpi=200

--- dompdf.php
+++ dompdf.php
@@ -13,7 +13,7 @@ $dompdf->loadHtml(file_get_contents('index.html'));
 $dompdf->setPaper('A4', 'portrait');
 
 // Other options
-$dompdf->set_option('dpi', '250');
+$dompdf->set_option('dpi', '200');
 
 // Render the HTML as PDF
 $dompdf->render();
