bd1cc8f2782005a3f624691b7e1e7a0e9113d6a7
Matthieu Borgognon <matrash@bqn.ch>
2018-04-20 22:38:14 +0200

    Try to change the pdfBackend to PDFLib

--- dompdf.php
+++ dompdf.php
@@ -13,7 +13,7 @@ $dompdf->loadHtml(file_get_contents('index.html'));
 $dompdf->setPaper('A4', 'portrait');
 
 // Other options
-$dompdf->set_option('dpi', '200');
+$dompdf->set_option('dpi', '195', 'pdfBackend', 'PDFLib');
 
 // Render the HTML as PDF
 $dompdf->render();
