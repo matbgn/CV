f8652dbd4b2a9965dc4807c9f2600881a730510e
Matthieu Borgognon <matrash@bqn.ch>
2018-04-20 21:08:09 +0200

    Fixed PDF ratio to (0,-500,1920,5000)

--- dompdf.php
+++ dompdf.php
@@ -10,7 +10,7 @@ $dompdf = new Dompdf();
 $dompdf->loadHtml(file_get_contents('index.html'));
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper(array(-500,-500,1920,1080), 'portrait');
+$dompdf->setPaper(array(0,-500,1920,5000), 'portrait');
 
 // Render the HTML as PDF
 $dompdf->render();
