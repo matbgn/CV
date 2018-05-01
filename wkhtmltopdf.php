17961a3fd8872ca7f26a428ef76651166882c06d
matbgn <matrash@bqn.ch>
2018-05-02 00:36:31 +0200

    Added a PDF name

--- wkhtmltopdf.php
+++ wkhtmltopdf.php
@@ -12,7 +12,7 @@ $pdf->addPage('https://matthieu.borgognon.ch');
 // On some systems you may have to set the path to the wkhtmltopdf executable
 $pdf->binary = './wkhtmltopdf';
 //Test to send
-$pdf->send();
+$pdf->send("MatthieuBorgognonCV.pdf");
 if (!$pdf->send()) {
     throw new Exception('Could not create PDF: '.$pdf->getError());
 }
