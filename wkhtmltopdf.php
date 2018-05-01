0d71822d98fbc098e35622459255a105aa527ae0
Matthieu Borgognon <matrash@bqn.ch>
2018-05-01 23:36:11 +0200

    Update and rename wkhtmltopdf.sh to wkhtmltopdf.php

---
+++ wkhtmltopdf.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use mikehaertl\wkhtmlto\Pdf;
+
+// You can pass a filename, a HTML string, an URL or an options array to the constructor
+$pdf = new Pdf(array(
+  'viewport-size' => 1280,
+));
+
+$pdf->addPage('https://matthieu.borgognon.ch');
+// On some systems you may have to set the path to the wkhtmltopdf executable
+$pdf->binary = '/$PWD/wkhtmltopdf/wkhtmltopdf';
+//Test to send
+$pdf->send();
+if (!$pdf->send()) {
+    throw new Exception('Could not create PDF: '.$pdf->getError());
+}
+?>
--- wkhtmltopdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./wkhtmltopdf http://google.com google.pdf
