50e271d38ee2955234db3eb7b76b4c68acccf0e3
Matthieu Borgognon <matrash@bqn.ch>
2018-05-01 21:54:01 +0200

    Update wkhtmltopdf.sh

--- wkhtmltopdf.sh
+++ wkhtmltopdf.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# $1 is the directory in which files to be processed
-# $2 is the css file
-for path in $(find $1 -name "*.html")
-do
-  echo -e "\033[92mProcessing ${path}\033[0m"
-  wkhtmltopdf ${path} --disable-javascript --user-style-sheet $2 "${path%.html}.pdf"
-done
+./wkhtmltopdf http://google.com google.pdf
